1ab6fc5c01a59ed9f98b29d02b663996c4bb25d4
nitinjha3 <nitinkumarvk100@gmail.com>
2024-01-27 13:43:53 +0530

    cache problem

--- script.js
+++ script.js
@@ -2,176 +2,185 @@ const togglebtn = document.getElementById('toggle-bar');
 const navLinks = document.getElementById('nav-links')
 
 togglebtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
+  navLinks.classList.toggle('active')
 })
 let btn = document.getElementsByClassName("btn")
 let slide = document.getElementById("slide")
 let counter = 0;
 btn[0].onclick = function () {
-    slide.style.transform = "translateX(0px)"
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active")
-    }
-    this.classList.add("active")
-    counter = 0;
+  slide.style.transform = "translateX(0px)"
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active")
+  }
+  this.classList.add("active")
+  counter = 0;
 }
 btn[1].onclick = function () {
-    slide.style.transform = "translateX(-800px)"
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active")
-    }
-    this.classList.add("active")
-    counter = 1;
+  slide.style.transform = "translateX(-800px)"
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active")
+  }
+  this.classList.add("active")
+  counter = 1;
 }
 btn[2].onclick = function () {
-    slide.style.transform = "translateX(-1600px)"
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active")
-    }
-    this.classList.add("active")
-    counter = 2;
+  slide.style.transform = "translateX(-1600px)"
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active")
+  }
+  this.classList.add("active")
+  counter = 2;
 }
 btn[3].onclick = function () {
-    slide.style.transform = "translateX(-2400px)"
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active")
-    }
-    this.classList.add("active")
-    counter = 3;
+  slide.style.transform = "translateX(-2400px)"
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active")
+  }
+  this.classList.add("active")
+  counter = 3;
 }
 
 // automatic
 
 setInterval(function () {
-    let val = -800;
-    // slide.style.transform="translateX(${0+val})"
-    slide.style.transform = `translateX(${0 + val * counter}px)`;
+  let val = -800;
+  // slide.style.transform="translateX(${0+val})"
+  slide.style.transform = `translateX(${0 + val * counter}px)`;
 
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active")
-    }
-    btn[counter].classList.add("active")
-    counter++;
-    if (counter == 4)
-        counter = 0
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active")
+  }
+  btn[counter].classList.add("active")
+  counter++;
+  if (counter == 4)
+    counter = 0
 
 }, 5000);
 
 
 //stats increasing
 function incrementDigit() {
-    var counterElement = document.getElementById('1');
-    // var currentDigit = parseInt(currentDigit.innerText, 10);
-    var currentDigit = parseInt(counterElement.innerText, 10);
+  var counterElement = document.getElementById('1');
+  // var currentDigit = parseInt(currentDigit.innerText, 10);
+  var currentDigit = parseInt(counterElement.innerText, 10);
 
-    if (currentDigit < 1000) {
-      currentDigit++;
-      counterElement.innerText = currentDigit;
-    }
+  if (currentDigit < 1000) {
+    currentDigit++;
+    counterElement.innerText = currentDigit;
   }
+}
 
-  // Automatically increment every second (1000 milliseconds)
-  setInterval(incrementDigit, .1);
+// Automatically increment every second (1000 milliseconds)
+setInterval(incrementDigit, .1);
 
 function incrementDigit2() {
-    var counterElement = document.getElementById('2');
-    // var currentDigit = parseInt(currentDigit.innerText, 10);
-    var currentDigit = parseInt(counterElement.innerText, 10);
+  var counterElement = document.getElementById('2');
+  // var currentDigit = parseInt(currentDigit.innerText, 10);
+  var currentDigit = parseInt(counterElement.innerText, 10);
 
-    if (currentDigit < 250) {
-      currentDigit++;
-      counterElement.innerText = currentDigit;
-    }
+  if (currentDigit < 250) {
+    currentDigit++;
+    counterElement.innerText = currentDigit;
   }
+}
 
-  // Automatically increment every second (1000 milliseconds)
-  setInterval(incrementDigit2, 10);
+// Automatically increment every second (1000 milliseconds)
+setInterval(incrementDigit2, 10);
 
 function incrementDigit3() {
-    var counterElement = document.getElementById('3');
-    // var currentDigit = parseInt(currentDigit.innerText, 10);
-    var currentDigit = parseInt(counterElement.innerText, 10);
+  var counterElement = document.getElementById('3');
+  // var currentDigit = parseInt(currentDigit.innerText, 10);
+  var currentDigit = parseInt(counterElement.innerText, 10);
 
-    if (currentDigit < 5000) {
-      currentDigit++;
-      counterElement.innerText = currentDigit;
-    }
+  if (currentDigit < 5000) {
+    currentDigit++;
+    counterElement.innerText = currentDigit;
   }
+}
 
-  // Automatically increment every second (1000 milliseconds)
-  setInterval(incrementDigit3, 10);
+// Automatically increment every second (1000 milliseconds)
+setInterval(incrementDigit3, 10);
 
 
 function incrementDigit5() {
-    var counterElement = document.getElementById('5');
-    // var currentDigit = parseInt(currentDigit.innerText, 10);
-    var currentDigit = parseInt(counterElement.innerText, 10);
+  var counterElement = document.getElementById('5');
+  // var currentDigit = parseInt(currentDigit.innerText, 10);
+  var currentDigit = parseInt(counterElement.innerText, 10);
 
-    if (currentDigit <150) {
-      currentDigit++;
-      counterElement.innerText = currentDigit;
-    }
+  if (currentDigit < 150) {
+    currentDigit++;
+    counterElement.innerText = currentDigit;
   }
+}
 
-  // Automatically increment every second (1000 milliseconds)
-  setInterval(incrementDigit5, 20);
+// Automatically increment every second (1000 milliseconds)
+setInterval(incrementDigit5, 20);
 
 
 function incrementDigit4() {
-    var counterElement = document.getElementById('4');
-    // var currentDigit = parseInt(currentDigit.innerText, 10);
-    var currentDigit = parseInt(counterElement.innerText, 10);
+  var counterElement = document.getElementById('4');
+  // var currentDigit = parseInt(currentDigit.innerText, 10);
+  var currentDigit = parseInt(counterElement.innerText, 10);
 
-    if (currentDigit <14) {
-      currentDigit++;
-      counterElement.innerText = currentDigit;
-    }
+  if (currentDigit < 14) {
+    currentDigit++;
+    counterElement.innerText = currentDigit;
   }
+}
 
-  // Automatically increment every second (1000 milliseconds)
-  setInterval(incrementDigit4, 200);
+// Automatically increment every second (1000 milliseconds)
+setInterval(incrementDigit4, 200);
 
 
 function incrementDigit6() {
-    var counterElement = document.getElementById('6');
-    // var currentDigit = parseInt(currentDigit.innerText, 10);
-    var currentDigit = parseInt(counterElement.innerText, 10);
+  var counterElement = document.getElementById('6');
+  // var currentDigit = parseInt(currentDigit.innerText, 10);
+  var currentDigit = parseInt(counterElement.innerText, 10);
 
-    if (currentDigit < 40) {
-      currentDigit++;
-      counterElement.innerText = currentDigit;
-    }
+  if (currentDigit < 40) {
+    currentDigit++;
+    counterElement.innerText = currentDigit;
   }
+}
 
-  // Automatically increment every second (1000 milliseconds)
-  setInterval(incrementDigit6, 100);
+// Automatically increment every second (1000 milliseconds)
+setInterval(incrementDigit6, 100);
 
 //   
 function scrollToSection(sectionId) {
-    var offset = 100; // Set the offset from the top
+  var offset = 100; // Set the offset from the top
 
-    var section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-    // window.scrollBy(0, -offset);
+  var section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
   }
+  // window.scrollBy(0, -offset);
+}
 
 
-  // popup
+// popup
 const container=document.getElementById('content')
   document.addEventListener('DOMContentLoaded', function() {
     setTimeout(()=>{
       document.getElementById('overlay').style.display = 'flex';
     container.classList.add('disable-content');
     document.body.classList.add('disable-scroll');
-    },2000)
+    },1000)
 });
 
 function closePopup() {
-    document.getElementById('overlay').style.display = 'none';
-    document.body.classList.remove('disable-scroll');
-    container.classList.remove('disable-content');
-    
+  document.getElementById('overlay').style.display = 'none';
+  document.body.classList.remove('disable-scroll');
+  container.classList.remove('disable-content');
+
+}
+function onDeleteAll() {
+  console.log("Deleted all history");
 }
 
+function deleteAllHistory() {
+  let deletingAll = browser.history.deleteAll();
+  deletingAll.then(onDeleteAll);
+}
+
+deleteAllHistory();
